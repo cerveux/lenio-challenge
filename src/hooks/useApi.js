9a431df2099881024c516efddf955c50e0276553
cerveux <diegomartinguzman@hotmail.com.ar>
2023-07-18 03:14:40 -0300

    feat: parallel fetching ready

--- src/hooks/useApi.js
+++ src/hooks/useApi.js
@@ -1,5 +1,6 @@
 import md5 from "md5";
 import { useProvider, useHeroDispatch } from "../context/AppContextProvider";
+import heroesJson from "./marvel.json"
 
 const baseUrl = "https://gateway.marvel.com/v1/public";
 const ts = Date().now;
@@ -10,18 +11,82 @@ const hash = md5(valueToHash);
 
 const query = `apikey=${publicKey}&ts=${ts}&hash=${hash}`;
 
-/* const queryString = `?ts=${timestamp}&apikey=${publickKey}&hash=${hash}`;
-const url = baseUrl + queryString;
-console.log(hash);
- */
+
 export function useApi() {
 
   const dispatch = useHeroDispatch();
 
-  async function getHeroes() {
+  async function get100Heroes(index) {
+
+    
+    const charactersUrl = `${baseUrl}/characters?offset=${index}&limit=100&${query}`;
+    const heroesCluster = await fetch(charactersUrl)
+      .then((res) => res.json())
+      .then((response) => response.data.results)
+      .then((characters) =>
+        characters.filter((character) => {
+          //erase unaviable images
+          return (
+            character.thumbnail.path !==
+              "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available" &&
+            character.thumbnail.path !==
+              "http://i.annihil.us/u/prod/marvel/i/mg/f/60/4c002e0305708"
+          );
+        })
+      )
+
+    return heroesCluster
+
+
+  }
+  
+  function getHeroes(params) {
+
+    const promises = []
+
+
+    for(let i=0 ; i <= 1550; i += 100){
+        promises.push(get100Heroes(i))
+    }
+
+    Promise.all(promises)
+    .then(results => results.flat())
+    .then((characters) =>
+        characters.filter((character) => {
+          //erase unaviable images
+          return (
+            character.thumbnail.path !==
+              "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available" &&
+            character.thumbnail.path !==
+              "http://i.annihil.us/u/prod/marvel/i/mg/f/60/4c002e0305708"
+          );
+        })
+      )
+      .then((res) => {
+        dispatch({
+          type: "added",
+          heroes: res,
+        });
+    })
+    .catch((error) => console.log(error));
+    
+  }
+
+  async function prueba() {
     const randomIndex = Math.floor(Math.random() * 1461);
     const charactersUrl = `${baseUrl}/characters?offset=${randomIndex}&limit=100&${query}`;
-    await fetch(charactersUrl)
+    console.log(heroesJson.data.results);
+
+
+
+    dispatch({
+        type: "added",
+        heroes: heroesJson.data.results
+    })
+
+    /* await fetch(heroesJson)
+    .then(res => console.log(res.data.results)) */
+    /* await fetch(heroesJson)
       .then((res) => res.json())
       .then((response) => response.data.results)
       .then((characters) =>
@@ -40,13 +105,8 @@ export function useApi() {
           type: "added",
           heroes: res,
         });
-
-        /* dispatch({
-            type: "new page",
-            paginate: res.slice(1, 20),
-          }); */
       })
-      .catch((error) => console.log(error));
+      .catch((error) => console.log(error)); */
     
 
     console.log("una llamada");
@@ -69,9 +129,6 @@ export function useApi() {
     return comicList;
   }
 
-  async function prueba(params) {
-    
-  }
 
   return { getHeroes, getComics, prueba };
 }
